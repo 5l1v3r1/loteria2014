3bb117356740406ff6d36ec8f5d3dc073958abd4
Nomex <david.lladro@gmail.com>
2014-12-19 13:17:15 +0100

    faltaba el while

--- loteria.sh
+++ loteria.sh
@@ -2,10 +2,13 @@
 
 numero=( "12345" "67890" )
 
-for i in "${numeros[@]}"
+while :
 do
-  PREMIO=`curl -s "http://api.elpais.com/ws/LoteriaNavidadPremiados?n=$i" | awk -F ":" {'print $3'} | awk -F "," {'print $1'}`
-  if [ $PREMIO -ne 0 ]; then
-     mplayer gol.mp3
-  fi
+   for i in "${numeros[@]}"
+   do
+     PREMIO=`curl -s "http://api.elpais.com/ws/LoteriaNavidadPremiados?n=$i" | awk -F ":" {'print $3'} | awk -F "," {'print $1'}`
+     if [ $PREMIO -ne 0 ]; then
+        mplayer gol.mp3
+     fi
+   done
 done
